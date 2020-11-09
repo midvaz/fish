1e95aa52cd11f5144f1f1999b781a0caeed89212
midvaz <mi28df@gmail.com>
2020-11-09 17:30:04 +0300

    v1.1

---
+++ fish/Assets/pixels/Scripts/MoveCamere.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class MoveCamere : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+    }
+}
+
---
+++ fish/Assets/pixels/Scripts/MovePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 2f;
+    private Rigidbody2D rb;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal"), moveY = Input.GetAxis("Vertical");
+        rb.MovePosition(rb.position + new Vector2(moveX, moveY) * speed * Time.deltaTime);
+
+    }
+}
