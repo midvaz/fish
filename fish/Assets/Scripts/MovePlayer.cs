822aca40eae21bf2d2f98d7d1cf67a3471310bea
Dytish <73854732+Dytish@users.noreply.github.com>
2020-11-13 00:47:14 +0300

    попка младенца
    чистый проект, почти как попка млоденца. хуйня с осью z все еще есть, как с этим жить, я не знаю. Все косяки и спрайты можешь найти в гугл диске, будут вопросы по папочкам, пиши)

---
+++ fish/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 2f;
+    public float jumpForce = 5f;
+    private Rigidbody2D rb;
+    public bool facingRight = true;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal"), moveY = Input.GetAxis("Vertical");
+        //rb.MovePosition(rb.position + new Vector2(moveX, moveY) * speed * Time.deltaTime);
+        rb.velocity = new Vector2(moveX * speed, moveY * speed); //так вроде не трясется и все норм
+
+        //дальше будет условие по разворону при хождение по x
+
+        if (facingRight==false && moveX > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveX < 0)
+        {
+            Flip();
+        }
+
+        //прыжок 
+
+        if ( Input.GetKeyDown(KeyCode.Space) )
+        {
+            rb.velocity = new Vector2(moveX * jumpForce, moveY * jumpForce);
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
---
+++ fish/Assets/Scripts/gan.cs
@@ -0,0 +1,35 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gan : MonoBehaviour
+{
+    public float offset;
+    public GameObject bullet;
+    public Transform shotPoint;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        //пуля
+        if (timeBtwShots <= 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(bullet, shotPoint.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
